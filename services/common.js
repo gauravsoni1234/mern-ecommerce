4fe3946bee8258bf34638fe456fe1a60dbc3c376
Your Name <you@example.com>
2024-06-25 11:00:58 +0530

    Backend : Vercel Deployment

--- services/common.js
+++ services/common.js
@@ -1,7 +1,7 @@
-const passport = require("passport");
+const passport = require('passport');
 
 exports.isAuth = (req, res, done) => {
-  return passport.authenticate("jwt");
+  return passport.authenticate('jwt');
 };
 
 exports.sanitizeUser = (user) => {
@@ -11,10 +11,9 @@ exports.sanitizeUser = (user) => {
 exports.cookieExtractor = function (req) {
   let token = null;
   if (req && req.cookies) {
-    token = req.cookies["jwt"];
+    token = req.cookies['jwt'];
   }
   //TODO : this is temporary token for testing without cookie
-  token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2NzdjODVkN2QxNDA3ZTMwMWViNmExMiIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzE5MTI2ODA4fQ.da1IkuVxQ6bnxZt2MXxOdXzXd1TgWLoTLmCorJzhkJ8";
+  // token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NWJmNzViOWI3OWFkMTNiMTUxNWQ0MCIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjgzNzQ4ODY3fQ.DyMEFgayuvUGrzvPHIxmCJWi4xstvp4hR-dRSBjRNhE"
   return token;
 };
