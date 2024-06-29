ad9ac009685d9cfcb818910470903b50f7f2bb97
Your Name <you@example.com>
2024-06-29 13:34:11 +0530

    Final email reset password

--- services/common.js
+++ services/common.js
@@ -1,7 +1,18 @@
-const passport = require('passport');
+const passport = require("passport");
+const nodemailer = require("nodemailer");
+
+let transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: "gaurav9414soni@gmail.com", // gmail
+    pass: process.env.MAIL_PASSWORD, // pass
+  },
+});
 
 exports.isAuth = (req, res, done) => {
-  return passport.authenticate('jwt');
+  return passport.authenticate("jwt");
 };
 
 exports.sanitizeUser = (user) => {
@@ -11,9 +22,18 @@ exports.sanitizeUser = (user) => {
 exports.cookieExtractor = function (req) {
   let token = null;
   if (req && req.cookies) {
-    token = req.cookies['jwt'];
+    token = req.cookies["jwt"];
   }
-  //TODO : this is temporary token for testing without cookie
-  // token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NWJmNzViOWI3OWFkMTNiMTUxNWQ0MCIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjgzNzQ4ODY3fQ.DyMEFgayuvUGrzvPHIxmCJWi4xstvp4hR-dRSBjRNhE"
   return token;
+};
+
+exports.sendMail = async function ({ to, subject, text, html }) {
+  let info = await transporter.sendMail({
+    from: '"E-commerce" <gaurav9414soni@gmail.com>', // sender address
+    to,
+    subject,
+    text,
+    html,
+  });
+  return info;
 };
